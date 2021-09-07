5fb51d580b2b68c27a990665124d69c7b6f49358
MaXiaoYang <396434855@qq.com>
2021-09-07 15:32:19 +0800

    添加Teacher类的框架

---
+++ teacher.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "Teacher.h"
+
+Teacher::Teacher()
+{
+
+}
+
+//有参构造
+Teacher::Teacher(int empId, string name, string pwd)
+{
+
+}
+
+//菜单界面
+void Teacher::openMenu()
+{
+
+}
+
+//查看所有预约
+void Teacher::showAllOrder()
+{
+	
+}
+
+//审核预约
+void Teacher::validOrder()
+{
+	
+}
