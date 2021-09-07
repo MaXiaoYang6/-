79376b85f0d1c61e7e92c71fe677cf8fa18d5900
MaXiaoYang <396434855@qq.com>
2021-09-07 10:17:09 +0800

    Modify 机房预约系统.cpp

--- 机房预约系统.cpp
+++ 机房预约系统.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+	cout << "======================  欢迎来到传智播客机房预约系统  ====================="
+		<< endl;
+	cout << endl << "请输入您的身份" << endl;
+	cout << "\t\t -------------------------------\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          1.学生代表           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          2.老    师           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          3.管 理 员           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          0.退    出           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t -------------------------------\n";
+	cout << "输入您的选择: ";
 
+	system("pause");
 	return 0;
 }
