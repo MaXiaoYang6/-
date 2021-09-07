e9a6936de06e9d9cd2ba7e49b33b58fdde8f66fb
MaXiaoYang <396434855@qq.com>
2021-09-07 16:53:56 +0800

    添加了登陆函数 loginIn

--- 机房预约系统.cpp
+++ 机房预约系统.cpp
@@ -1,5 +1,70 @@
 #include <iostream>
 using namespace std;
+#include "Identity.h"
+#include <fstream>
+#include <string>
+#include "globalFile.h"
+
+//登陆功能
+//参数1 操作文件名 参数2 操作的身份类型
+void LoginIn(string fileName, int type)
+{
+	Identity* person = NULL;
+
+	ifstream ifs;
+	ifs.open(fileName, ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件不存在" << endl;
+		ifs.close();
+		return;
+	}
+
+	//准备接受用户的信息
+	int id = 0;
+	string name;
+	string pwd;
+
+	//判断身份
+	if (type == 1)
+	{
+		//学生
+		cout << "请输入你的学号:" << endl;
+		cin >> id;
+	}
+	else if (type == 2)
+	{
+		//老师
+		cout << "请输入你的职工号:" << endl;
+		cin >> id;
+	}
+
+	cout << "请输入用户名：" << endl;
+	cin >> name;
+
+	cout << "请输入密码：" << endl;
+	cin >> pwd;
+
+	if (type == 1)
+	{
+		//学生身份验证
+
+	}
+	else if (type == 2)
+	{
+		//教师身份验证
+	}
+	else if (type == 3)
+	{
+		//管理员身份验证
+	}
+
+	cout << "验证登陆失败" << endl;
+	system("pause");
+	system("cls");
+	return;
+}
 
 int main()
 {
@@ -27,20 +92,14 @@ int main()
 		switch (select)
 		{
 		case 1://Student
-
-			system("cls");
+			LoginIn(STUDENT_FILE, 1);
 			break;
-
 		case 2://Teacher
-
-			system("cls");
+			LoginIn(TEACHER_FILE, 2);
 			break;
-
 		case 3://Admin
-
-			system("cls");
+			LoginIn(ADMIN_FILE, 3);
 			break;
-
 		case 0://Exit
 			cout << "欢迎下次使用！" << endl;
 			return 0;
