a11b9048a6c5ce6220c862a196b1a883a78d3a0e
MaXiaoYang <396434855@qq.com>
2021-09-08 21:53:25 +0800

    no message

--- Manager.h
+++ Manager.h
@@ -2,6 +2,12 @@
 #include <iostream>
 using namespace std;
 #include "Identity.h"
+#include <fstream>
+#include <vector>
+#include "Student.h"
+#include "Teacher.h"
+#include <algorithm>
+#include "computerRoom.h"
 
 class Manager : public Identity
 {
@@ -27,4 +33,21 @@ public:
 	
 	//清空预约记录
 	void cleanFile();
+
+	//初始化容器
+	void initVector();
+
+	//检测重复 参数（传入id，传入类型） 返回值True or False
+	bool checkRepeat(int id, int type);
+
+
+
+	//学生容器
+	vector<Student> vStu;
+
+	//教师容器
+	vector<Teacher> vTea;
+
+	//机房信息
+	vector<ComputerRoom> vCom;
 };
--- manager.cpp
+++ manager.cpp
@@ -1,4 +1,8 @@
 #include "Manager.h"
+#include "globalFile.h"
+#include "Student.h"
+#include "Teacher.h"
+
 
 //默认构造
 Manager::Manager()
@@ -11,6 +15,19 @@ Manager::Manager(string name, string pwd)
 {
 	this->m_Name = name;
 	this->m_Pwd = pwd;
+	this->initVector();
+	
+	//初始化机房信息
+	ifstream ifs;
+	ifs.open(COMPUTER_FILE, ios::in);
+
+	ComputerRoom com;
+	while (ifs >> com.m_ComId && ifs >> com.m_MaxNum)
+	{
+		vCom.push_back(com);
+	}
+	ifs.close();
+	cout << "机房的数量为：" << vCom.size() << endl;
 }
 
 //菜单界面
@@ -36,23 +53,184 @@ void Manager::openMenu()
 //添加账号
 void Manager::addPerson()
 {
+	cout << "请输入添加账号的类型" << endl;
+	cout << "1.添加学生" << endl;
+	cout << "2.添加老师" << endl;
 
+	string fileName;//操作文件名
+	string tip;//提示id号
+	string errTip;
+	ofstream ofs;//文件操作对象
+
+	int select = 0;
+	cin >> select;
+
+	if (select == 1)
+	{
+		//添加的是学生
+		fileName = STUDENT_FILE;
+		tip = "请输入学号:";
+		errTip = "学号重复！请重新输入：";
+	}
+	else
+	{
+		fileName = TEACHER_FILE;
+		tip = "请输入职工编号:";
+		errTip = "职工编号重复！请重新输入：";
+	}
+
+	//利用追加的方式 写文件
+	ofs.open(fileName, ios::out | ios::app);
+
+	int id;
+	string name;
+	string pwd;
+
+	cout << tip;
+
+	while (true)
+	{
+		cin >> id;
+		bool ret = checkRepeat(id, select);
+		if (ret)
+		{
+			cout << errTip << endl;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	cout << "请输入姓名：" << endl;
+	cin >> name;
+
+	cout << "请输入密码：" << endl;
+	cin >> pwd;
+
+	//向文件中添加数据
+	ofs << id << " " << name << " " << pwd << " " << endl;
+	cout << "添加成功！" << endl;
+
+	system("pause");
+	system("cls");
+
+	ofs.close();
+	this->initVector();
+}
+
+void printStudent(Student& s)
+{
+	cout << s.m_Id << " " << s.m_Name << " " << s.m_Pwd << endl;
+}
+
+void printTeacher(Teacher& t)
+{
+	cout << t.m_EmpId << " " << t.m_Name << " " << t.m_Pwd << endl;
 }
 
 //查看账号
 void Manager::showPerson()
 {
+	cout << "请选择查看的内容：" << endl;
+	cout << "1.查看所有老师" << endl;
+	cout << "2.查看所有学生" << endl;
 
+	int select = 0;//接收用户选择
+	cin >> select;
+
+	if (select == 1)
+	{
+		//查看学生
+		cout << "所有学生的信息如下：" << endl;
+		for_each(vStu.begin(), vStu.end(), printStudent);
+	}
+	else
+	{
+		//查看老师
+		cout << "所有老师的信息如下：" << endl;
+		for_each(vTea.begin(), vTea.end(), printTeacher);
+	}
+
+	system("pause");
+	system("cls");
 }
 
 //查看机房信息
 void Manager::showComputer()
 {
-
+	cout << "机房信息如下:" << endl;
+	for (vector<ComputerRoom>::iterator it = vCom.begin(); it != vCom.end(); it++)
+	{
+		cout << "机房编号：" << it->m_ComId << " 机房最大容量：" << it->m_MaxNum << endl;
+	}
+	system("pause");
+	system("cls");
 }
 
 //清空预约记录
 void Manager::cleanFile()
 {
 
+}
+
+void Manager::initVector()
+{
+	//确保容器为空
+	vStu.clear();
+	vTea.clear();
+
+	//读取信息 学生
+	ifstream ifs;
+	ifs.open(STUDENT_FILE, ios::in);
+	if (!ifs.is_open())
+	{
+		cout << "文件读取失败" << endl;
+		return;
+	}
+
+	Student s;
+	while (ifs >> s.m_Id && ifs >> s.m_Name && ifs >> s.m_Pwd)
+	{
+		vStu.push_back(s);
+	}
+
+	cout << "当前学生数量为:" << vStu.size() << endl;
+	ifs.close();
+
+	//读取信息 老师
+	ifs.open(TEACHER_FILE, ios::in);
+	Teacher t;
+	while (ifs >> t.m_EmpId && ifs >> t.m_Name &&
+		ifs >> t.m_Pwd)
+	{
+		vTea.push_back(t);
+	}
+	cout << "当前老师的数量为：" << vTea.size() << endl;
+	ifs.close();
+}
+
+bool Manager::checkRepeat(int id, int type)
+{
+	if (type == 1)
+	{//检测学生
+		for (vector<Student>::iterator it = vStu.begin(); it != vStu.end(); it++)
+		{
+			if (id == it->m_Id)
+			{
+				return true;
+			}
+		}
+	}
+	else
+	{
+		for (vector<Teacher>::iterator it = vTea.begin(); it != vTea.end(); it++)
+		{
+			if (id == it->m_EmpId)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
